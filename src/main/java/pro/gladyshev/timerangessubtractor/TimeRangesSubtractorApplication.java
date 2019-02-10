04b0472f423b9cdacb3ea98be1693d7b2011c760
dgladyshev <dmitry.gladishev@gmail.com>
2019-02-10 15:02:21 +0700

    Initial commit: Spring Boot 2, Gradle, Dependencies

---
+++ src/main/java/pro/gladyshev/timerangessubtractor/TimeRangesSubtractorApplication.java
@@ -0,0 +1,14 @@
+package pro.gladyshev.timerangessubtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRangesSubtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeRangesSubtractorApplication.class, args);
+	}
+
+}
+
